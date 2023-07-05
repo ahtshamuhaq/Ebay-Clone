de8adc8878880011eab167fe2329bbd745c13b8d
Ahtsham Ul Haq <112152384+ahtshamuhaq@users.noreply.github.com>
2023-07-05 07:35:48 +0500

    added pages

---
+++ src/pages/MoneyBack/MoneyBackGuarantee.js
@@ -0,0 +1,11 @@
+import React from "react";
+import 
+const MoneyBackGuarantee = () => {
+  return <div>
+    <div>
+        // <img src={} alt="" />
+    </div>
+  </div>;
+};
+
+export default MoneyBackGuarantee;
